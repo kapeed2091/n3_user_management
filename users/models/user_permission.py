94c983c06e6ec71f3e5532dd5614b6e8a9fdd9ee
Deepak Kumar Malladi <kapeed2091@gmail.com>
2021-03-07 20:54:18 +0530

    Change ForeignKey references to name string from model object

--- users/models/user_permission.py
+++ users/models/user_permission.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 from .abstract_datetime import AbstractDatetime
-from .permission import Permission
-from .user import User
 
 
 class UserPermission(AbstractDatetime):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    permission = models.ForeignKey("users.Permission", on_delete=models.CASCADE)
