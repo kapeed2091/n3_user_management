7891705bb0bf5746bcb000805ddcd610bf28567c
Deepak Kumar Malladi <kapeed2091@gmail.com>
2021-03-08 19:51:42 +0530

    Add user profile update API

--- users/views/user_profile_view.py
+++ users/views/user_profile_view.py
@@ -23,9 +23,38 @@ class UserSerializer(serializers.ModelSerializer):
         ]
 
 
-@api_view(["GET"])
+class UserProfileUpdateRequestSerializer(serializers.Serializer):
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    phone = serializers.CharField(required=False)
+    street = serializers.CharField(required=False)
+    city = serializers.CharField(required=False)
+    state = serializers.CharField(required=False)
+    zipcode = serializers.CharField(required=False)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.phone = validated_data.get("phone", instance.phone)
+        instance.street = validated_data.get("street", instance.street)
+        instance.city = validated_data.get("city", instance.city)
+        instance.state = validated_data.get("state", instance.state)
+        instance.zipcode = validated_data.get("zipcode", instance.zipcode)
+        instance.save()
+        return instance
+
+
+@api_view(["GET", "PUT"])
 @authentication_classes([JWTAuthentication])
 def user_profile_view(request):
-    user = request.user
-    serializer = UserSerializer(user)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == "GET":
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        user = request.user
+        serializer = UserProfileUpdateRequestSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=400)
